fb0b5e359da496d87d8a05e9e544a7a6fa28a8d9
Madelaine <madelaine.leitman@gmail.com>
2024-07-01 15:13:00 -0700

    Add data to github

--- edge_count.py
+++ edge_count.py
@@ -4,9 +4,9 @@ import pandas as pd
 import numpy as np
 
 #%%
-# Uplaod data
-edge_df = pd.read_table(filepath_or_buffer='/Users/madelaineleitman/Downloads/KnowlesLab/my_network_with_db.ntw', sep=' ', header=None)
-network_labels_df = pd.read_csv(filepath_or_buffer='/Users/madelaineleitman/Downloads/KnowlesLab/useme_updated_network_table_labels.csv')
+# Upload data
+edge_df = pd.read_table(filepath_or_buffer='/Users/madelaineleitman/Downloads/KnowlesLab/Viral_Genomics/edge_weights.ntw', sep=' ', header=None)
+network_labels_df = pd.read_csv(filepath_or_buffer='/Users/madelaineleitman/Downloads/KnowlesLab/Viral_Genomics/network_labels.csv')
 
 
 #%%
@@ -15,3 +15,6 @@ edge_df_genome_col = edge_df[[0]]
 network_labels_df_genome_col = network_labels_df[['Genome']]
 df = pd.merge(edge_df, network_labels_df, left_on=0, right_on='Genome', how='left')
 df.rename(columns={0:'Source', 1:'Target', 2:'edge_weight'}, inplace=True)
+
+
+
